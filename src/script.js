a906ef5c9a7dcf0ad771622400dca174b16f7d8f
Fabio Loche <hmll86f3@students.codeinstitute.net>
2024-10-18 20:26:44 +0000

    Add shuffleArray function to randomize quiz questions in script.js

--- src/script.js
+++ src/script.js
@@ -322,3 +322,10 @@ function reviewAnswers() {
     document.getElementById("resultScreen").classList.add("hidden");
     document.getElementById("screen4").classList.remove("hidden");
 }
+
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
